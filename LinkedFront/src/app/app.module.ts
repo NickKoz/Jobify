2d573aed8088bf79de47ffbd1b6f1bdf16cdcc4f
Nikos Kozios <44836357+NickKoz@users.noreply.github.com>
2021-08-05 02:21:07 +0300

    Adding basic components for website: header, footer, homepage and not found page

--- LinkedFront/src/app/app.module.ts
+++ LinkedFront/src/app/app.module.ts
@@ -5,16 +5,43 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', 
+        component: HomeComponent
+      },
+      {
+        path: 'register', 
+        component: RegisterComponent
+      },
+      {
+        path: '404', 
+        component: NotFoundComponent
+      },
+      {
+        path: '**', 
+        redirectTo: '404'
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
